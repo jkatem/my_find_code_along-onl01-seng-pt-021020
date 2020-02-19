fbe1d00a2f63d29ac7ad8379726c1e13a566030d
Jen Miao <jenmiao@gmail.com>
2020-02-19 22:47:23 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0
+  while i < collection.length
+    if yield(collection[i])
+      return collection[i]
+    ##can simplify by writing like this:
+    #return collection[i] if yield(collection[i])
+    end
+    i+=1
+  end
 end
